7787f8240e6892bbe1bad1eae5d374171130b518
Roman <hpp1ovh@gmail.com>
2025-10-08 14:18:14 +0300

    complite task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+spider.style.left = (wall.clientWidth - spiderWidth) / 2 + 'px';
+spider.style.top = (wall.clientHeight - spiderHeight) / 2 + 'px';
